05487614c1bd96b153339b62d8cb22a0461f0fc5
Joan <joanalemany26@gmail.com>
2024-04-23 17:53:07 +0200

    added scroll to top button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const headerMenuImg = document.querySelector('.header__menu__img');
 const headerDropdown = document.querySelector('.header__dropdown');
 const header = document.querySelector('.header');
 const percentageScroller = document.querySelector('.percentage-scroller');
+const scrollTopButton = document.querySelector('.scroll-top__button');
 
 function toggleDropdown(headerDropdown, header) {
   headerDropdown.classList.toggle('disabled');
@@ -23,15 +24,35 @@ function setPercentageScrollBar(scrollPercentage, percentageScroller) {
   percentageScroller.style.width = scrollPercentage + '%';
 }
 
+function handleScrollTopButtonOpacity(scrollPercentage, scrollTopButton) {
+  if(scrollPercentage < 95){
+    scrollTopButton.style.opacity = 0;
+  }else {
+    scrollTopButton.style.opacity = 1;
+  }
+}
+
+function handleScrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+
 headerMenu.addEventListener('click', function() {
   toggleDropdown(headerDropdown, header);
   switchMenuIcon(headerMenuImg);
+});
+
+scrollTopButton.addEventListener('click', function() {
+  handleScrollToTop();
 });
 
 document.addEventListener('scroll', function() {
   const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
   const scrollPosition = document.documentElement.scrollTop;
   const scrollPercentage = Math.floor((scrollPosition/maxScroll) * 100);
-
+  handleScrollTopButtonOpacity(scrollPercentage, scrollTopButton)
   setPercentageScrollBar(scrollPercentage, percentageScroller)
 });
