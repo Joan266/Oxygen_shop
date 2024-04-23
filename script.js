af50bf1b105f35b703a440bac7cb057efac5ac02
Joan <joanalemany26@gmail.com>
2024-04-23 12:58:06 +0200

    when you click the header menu icon it opens the dropdown showing all the sections and switch to cross icon so it can be close

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-document.querySelector('.header__menu').addEventListener('click', function() {
-  console.log("hello")
+const headerMenu = document.querySelector('.header__menu');
+const headerMenuImg = document.querySelector('.header__menu__img');
+const headerDropdown = document.querySelector('.header__dropdown');
+const header = document.querySelector('.header');
+const menuFilePath = "./public/svg/menu.svg";
+const xFilePath = "./public/svg/x.svg";
+const menuAltString = "menu icon";
+const xAltString = "x icon";
+headerMenu.addEventListener('click', function() {
+  headerDropdown.classList.toggle('disabled');
+  header.classList.toggle('boxshadow-disabled');
+
+  if (headerMenuImg.getAttribute('src') === menuFilePath) {
+    headerMenuImg.setAttribute('src', xFilePath);
+    headerMenuImg.setAttribute('alt', xAltString);
+  } else {
+    headerMenuImg.setAttribute('src', menuFilePath);
+    headerMenuImg.setAttribute('alt', menuAltString);
+  }
+  console.log("Header Dropdown Classes: ", headerDropdown.classList);
+  console.log("Header Classes: ", header.classList);
+  console.log("Header Menu Classes: ", headerMenu.classList);
 });
