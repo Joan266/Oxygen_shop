0ca3649f8ccd9dc77a9902e2e5f977a45f7175be
Joan <joanalemany26@gmail.com>
2024-04-24 16:15:27 +0200

    added form validation and submit listener

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const headerMenu = document.querySelector('.header__menu');
 const headerMenuImg = document.querySelector('.header__menu__img');
 const headerDropdown = document.querySelector('.header__dropdown');
 const header = document.querySelector('.header');
+
 const percentageScroller = document.querySelector('.percentage-scroller');
 const scrollTopButton = document.querySelector('.scroll-top__button');
+
+const form = document.getElementById('contactForm');
+const nameInput = document.getElementById("contactFormName");
+const emailInput = document.getElementById("contactFormEmail");
+const checkbox = document.getElementById("contactFormCheckbox");
 
 function toggleDropdown(headerDropdown, header) {
   headerDropdown.classList.toggle('disabled');
@@ -39,6 +45,60 @@ function handleScrollToTop() {
   });
 }
 
+function resetFormInputClasses() {
+  nameInput.classList.remove("feedback", "feedback--error", "feedback--success");
+  emailInput.classList.remove("feedback", "feedback--error", "feedback--success");
+  checkbox.classList.remove("feedback", "feedback--error", "feedback--success");
+}
+
+function validateForm() {
+  let isValid = true;
+  const nameLength = nameInput.value.trim().length;
+  if (nameLength < 2 || nameLength > 100) {
+    isValid = false;
+    setErrorInputClass(nameInput);
+  } else {
+    setSuccessInputClass(nameInput);
+  }
+
+  if (!isEmail(emailInput.value.trim())) {
+    isValid = false;
+    setErrorInputClass(emailInput);
+  } else {
+    setSuccessInputClass(emailInput);
+  }
+
+  if (!checkbox.checked) {
+    isValid = false;
+    setErrorInputClass(checkbox);
+  } else {
+    setSuccessInputClass(checkbox);
+  }
+  return isValid;
+}
+
+function setErrorInputClass(input) {
+  input.classList.add("feedback", "feedback--error");
+  input.classList.remove("feedback--success");
+}
+
+function setSuccessInputClass(input) {
+  input.classList.add("feedback", "feedback--success");
+  input.classList.remove("feedback--error");
+}
+
+function isEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (validateForm()) {
+    resetFormInputClasses();
+    alert("Form submitted successfully!");
+    form.reset();
+  }
+});
 
 headerMenu.addEventListener('click', function() {
   toggleDropdown(headerDropdown, header);
