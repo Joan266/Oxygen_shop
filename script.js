3962f09ca1bdf7ac9eb25c979f2a9dcd106bf95c
Joan <joanalemany26@gmail.com>
2024-04-26 12:08:12 +0200

    added change currency selector and js script

--- script.js
+++ script.js
@@ -1,16 +1,100 @@
+//MENU ELEMENTS
 const headerMenu = document.querySelector('.header__menu');
 const headerMenuImg = document.querySelector('.header__menu__img');
 const headerDropdown = document.querySelector('.header__dropdown');
 const header = document.querySelector('.header');
 
+//SCROLL ELEMENTS
 const percentageScroller = document.querySelector('.percentage-scroller');
 const scrollTopButton = document.querySelector('.scroll-top-button');
 
+//FORM ELEMENTS
 const form = document.getElementById('contactForm');
 const nameInput = document.getElementById("contactFormName");
 const emailInput = document.getElementById("contactFormEmail");
 const checkbox = document.getElementById("contactFormCheckbox");
 
+//CURRENCY ELEMENTS
+const princingCurrencySelector = document.querySelector('.pricing__currency-selector');
+const basicPriceElement = document.getElementById('basic-price');
+const professionalPriceElement = document.getElementById('professional-price');
+const premiumPriceElement = document.getElementById('premium-price');
+
+let subcriptions = JSON.parse(localStorage.getItem('subcriptions'));
+
+//EVENT LISTENERS
+princingCurrencySelector.addEventListener("change", async (event) => {
+  const selectedCurrency = event.target.value;
+  if (!subcriptions) {
+    console.log('Subscription prices not available.');
+    await calculateCurrency();
+  }
+  const convertPrice = (price) => {
+    switch (selectedCurrency) {
+      case 'eur':
+        return price.eur;
+      case 'usd':
+        return price.usd;
+      case 'gbp':
+        return price.gbp;
+      default:
+        return price.usd; 
+    }
+  };
+
+  basicPriceElement.textContent = convertPrice(subcriptions.basic);
+  professionalPriceElement.textContent = convertPrice(subcriptions.professional);
+  premiumPriceElement.textContent = convertPrice(subcriptions.premium);
+});
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const isContentFormValid = validateForm(nameInput, emailInput, checkbox)
+  if (isContentFormValid) {
+    const validFormContent = {
+      name: nameInput.value,
+      email: emailInput.value,
+    }
+    await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify(validFormContent),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      if(json){
+        alert("Form submitted successfully!");
+      }
+    });
+    resetFormInputClasses(nameInput, emailInput, checkbox);
+    form.reset();
+  }
+});
+
+headerMenu.addEventListener('click', function(event) {
+  event.preventDefault();
+  toggleDropdown(headerDropdown, header);
+  switchMenuIcon(headerMenuImg);
+});
+
+scrollTopButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  handleScrollToTop();
+});
+
+document.addEventListener('scroll', function() {
+  const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPosition = document.documentElement.scrollTop;
+  const scrollPercentage = Math.floor((scrollPosition/maxScroll) * 100);
+  handleScrollTopButtonOpacity(scrollPercentage, scrollTopButton)
+  setPercentageScrollBar(scrollPercentage, percentageScroller)
+});
+
+
+//MENU FUNCTIONS
 function toggleDropdown(headerDropdown, header) {
   headerDropdown.classList.toggle('disabled');
   header.classList.toggle('boxshadow-disabled');
@@ -26,6 +110,7 @@ function switchMenuIcon(headerMenuImg) {
   }
 }
 
+//SCROLL FUNCTIONS
 function setPercentageScrollBar(scrollPercentage, percentageScroller) {
   percentageScroller.style.width = scrollPercentage + '%';
 }
@@ -50,13 +135,28 @@ function handleScrollToTop() {
   });
 }
 
-function resetFormInputClasses() {
+//FORM FUNCTIONS
+function setErrorInputClass(input) {
+  input.classList.add("feedback", "feedback--error");
+  input.classList.remove("feedback--success");
+}
+
+function setSuccessInputClass(input) {
+  input.classList.add("feedback", "feedback--success");
+  input.classList.remove("feedback--error");
+}
+
+function isEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function resetFormInputClasses(nameInput, emailInput, checkbox) {
   nameInput.classList.remove("feedback", "feedback--error", "feedback--success");
   emailInput.classList.remove("feedback", "feedback--error", "feedback--success");
   checkbox.classList.remove("feedback", "feedback--error", "feedback--success");
 }
 
-function validateForm() {
+function validateForm(nameInput, emailInput, checkbox) {
   let isValid = true;
   const nameLength = nameInput.value.trim().length;
   if (nameLength < 2 || nameLength > 100) {
@@ -82,62 +182,53 @@ function validateForm() {
   return isValid;
 }
 
-function setErrorInputClass(input) {
-  input.classList.add("feedback", "feedback--error");
-  input.classList.remove("feedback--success");
-}
-
-function setSuccessInputClass(input) {
-  input.classList.add("feedback", "feedback--success");
-  input.classList.remove("feedback--error");
-}
-
-function isEmail(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
-
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const isContentFormValid = validateForm()
-  if (isContentFormValid) {
-    const validFormContent = {
-      name: nameInput.value,
-      email: emailInput.value,
+//CURRENCY FUNCTIONS 
+async function fetchCurrency() {
+  try {
+    const response = await fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json');
+    if(response.ok) {
+      const json = await response.json();
+      
+      return {
+        usd: json.eur.usd,
+        gbp: json.eur.gbp
+      };
     }
-    await fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      body: JSON.stringify(validFormContent),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json);
-      if(json){
-        alert("Form submitted successfully!");
-      }
-    });
-    resetFormInputClasses();
-    form.reset();
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
   }
-});
+}
 
-headerMenu.addEventListener('click', function(event) {
-  event.preventDefault();
-  toggleDropdown(headerDropdown, header);
-  switchMenuIcon(headerMenuImg);
-});
 
-scrollTopButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  handleScrollToTop();
-});
 
-document.addEventListener('scroll', function() {
-  const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
-  const scrollPosition = document.documentElement.scrollTop;
-  const scrollPercentage = Math.floor((scrollPosition/maxScroll) * 100);
-  handleScrollTopButtonOpacity(scrollPercentage, scrollTopButton)
-  setPercentageScrollBar(scrollPercentage, percentageScroller)
-});
+function convertAmount(amount, currencyRates) {
+  return {
+    eur: "€" + Math.floor(amount / currencyRates.usd),
+    usd: "$" + amount,
+    gbp: "£" + Math.floor((amount / currencyRates.usd) * currencyRates.gbp),
+  };
+}
+
+async function calculateCurrency() {
+  let currencyRates;
+
+  if (!subcriptions) {
+    currencyRates = await fetchCurrency();
+    if (!currencyRates) {
+      console.log('Failed to fetch currency rates.');
+      return;
+    }
+    subcriptions = {
+      basic: convertAmount(0, currencyRates),
+      professional: convertAmount(25, currencyRates),
+      premium: convertAmount(60, currencyRates),
+    }
+    localStorage.setItem('subcriptions', JSON.stringify(subcriptions));
+  }
+  
+  console.log('Subcriptions:', subcriptions);
+}
+
+
+calculateCurrency();
