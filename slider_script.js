e59427e6126c9d7a2806660c96432f24323a4fbc
Joan <joanalemany26@gmail.com>
2024-04-29 12:36:55 +0200

    added picsum img folder

--- slider_script.js
+++ slider_script.js
@@ -1,43 +1,9 @@
-async function generateImages() {
-  // Clear previous imageArray storage
-  localStorage.removeItem("imageArray");
+const slider = document.getElementById("slider");
+const sliderImg = document.querySelector(".slider__img");
+const leftButton = document.querySelector(".slider__button--left");
+const rightButton = document.querySelector(".slider__button--right");
+const imgSelector = document.querySelector(".slider__img-selector");
+const imgSelectorOptions = document.querySelectorAll(".slider__img-selector__option");
+const imgSelectorDotes = document.querySelectorAll(".slider__img-selector__option__dote");
 
-  const imageArray = [];
-  let imgId = Math.floor(Math.random() * 1000);
 
-  for (let i = imgId; i < imgId + 5; i++) {
-    const images = await generateImageResources(i);
-    imageArray.push(images);
-  }
-
-  // Store image array in local storage
-  localStorage.setItem("imageArray", JSON.stringify(imageArray));
-}
-
-async function generateImageResources(imgId) {
-  // Generate image URLs
-  const desktopUrl = `https://picsum.photos/id/${imgId}/800/600.webp`;
-  const mobileUrl = `https://picsum.photos/id/${imgId}/400/300.webp`;
-
-  const responseDesktop = await fetch(desktopUrl);
-  const responseMobile = await fetch(mobileUrl);
-  
-  const blobDesktop = await responseDesktop.blob();
-  const blobMobile = await responseMobile.blob();
-  
-  const base64dataDesktop = await convertToBase64(blobDesktop);
-  const base64dataMobile = await convertToBase64(blobMobile);
-
-  return { computer: base64dataDesktop, mobile: base64dataMobile };
-}
-
-async function convertToBase64(blob) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(blob);
-    reader.onloadend = () => resolve(reader.result);
-  });
-}
-
-// Call the function to generate images
-generateImages();
